8e9793ca3d04deba8e0155b0c3c9ec48ce85c9d0
KaterynaManko <k.manko16@gmail.com>
2023-03-26 15:00:30 +0300

    Add changes to blog

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, session, flash
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
+import functools
+
+def login_required(view_func):
+   @functools.wraps(view_func)
+   def check_permissions(*args, **kwargs):
+       if session.get('logged_in'):
+           return view_func(*args, **kwargs)
+       return redirect(url_for('login', next=request.path))
+   return check_permissions
 
 @app.route("/")
 def index():
@@ -9,25 +18,89 @@ def index():
 
    return render_template("homepage.html", all_posts=all_posts)
 
-@app.route("/new-post/<select>/<entry_id>", methods=["GET", "POST"])
-def create_entry(select, entry_id):
+@app.route("/new-post/", methods=["GET", "POST"])
+@login_required
+def create_entry():
    form = EntryForm()
    errors = None
    if request.method == 'POST':
-     if form.validate_on_submit():
-      if select == 'create':
+    if form.validate_on_submit():
             entry = Entry(
             title=form.title.data,
             body=form.body.data,
             is_published=form.is_published.data)
             db.session.add(entry)
-      if select == 'edit':
-           entry = Entry.query.filter_by(id=entry_id).first_or_404()
-           form.populate_obj(entry)
-      db.session.commit() 
-     else:
+            db.session.commit()
+    else:
            errors = form.errors
+    return redirect(url_for("index"))        
    return render_template("entry_form.html", form=form, errors=errors)
 
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
+def edit_entry(entry_id):
+   entry = Entry.query.filter_by(id=entry_id).first_or_404()
+   form = EntryForm(obj=entry)
+   errors = None
+   if request.method == 'POST':
+       if form.validate_on_submit():
+            form.populate_obj(entry)
+            db.session.commit()
+       else:
+           errors = form.errors
+       return redirect(url_for("index"))  
+   return render_template("edit_form.html", form=form, errors=errors)
+   
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index')) 
+
+@app.route("/drafts/", methods=['GET'])
+@login_required
+def list_drafts():
+   drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+   return render_template("drafts.html", drafts=drafts)
+
+@app.route("/delete-draft/<int:entry_id>", methods=["GET", "POST"])
+@login_required
+def delete_draft(entry_id):
+   entry = Entry.query.filter_by(id=entry_id).first_or_404()
+   drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+   if entry in drafts:
+         db.session.delete(entry)
+         db.session.commit()
+         flash('Delete draft')    
+   return redirect(url_for("index"))
+
+@app.route("/delete-entry/<int:entry_id>", methods=["GET", "POST"])
+@login_required
+def delete_entry(entry_id):
+   entry = Entry.query.filter_by(id=entry_id).first_or_404()
+   all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+   if entry in all_posts:
+         db.session.delete(entry)
+         db.session.commit()
+         flash('Delete entry')    
+   return redirect(url_for("index"))
+       
+   
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))    
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
    SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"    
@@ -9,3 +9,5 @@ class Config:
            'sqlite:///' + os.path.join(BASE_DIR, 'blog.db')
    )
    SQLALCHEMY_TRACK_MODIFICATIONS = False
+   ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+   ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change-me") 
